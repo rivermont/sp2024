7e7eaa01b78a86b26fb52ca6b24c28b4808b72ca
rivemont <william.11bennett@gmail.com>
2024-02-26 13:34:46 -0500

    5818 Add min working map for lab 3.

---
+++ lab03/GrandfatherStops.js
@@ -0,0 +1,198 @@
+var stops = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Church Road, Foscoe",
+        "desc": "",
+        "url": "https://ebird.org/checklist/S138685552"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -81.7648094,
+          36.1498726
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Wilson Center for Nature Discovery",
+        "desc": "",
+        "url": "https://ebird.org/checklist/S138502812"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -81.8356464,
+          36.0902426
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Beacon Heights",
+        "desc": "",
+        "url": "https://ebird.org/checklist/S138691218"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -81.8301267,
+          36.0838728
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Riverbank Cove",
+        "desc": ""
+      },
+      "geometry": {
+        "coordinates": [
+          -81.76783974825727,
+          36.15710580616435
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Watauga 105 Recycling Center",
+        "desc": "",
+        "url": "https://ebird.org/checklist/S138686497"
+      },
+      "geometry": {
+        "coordinates": [
+          -81.78994320193067,
+          36.149926834446774
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Wetlands behind Grandview",
+        "desc": "",
+        "url": "https://ebird.org/checklist/S138686890"
+      },
+      "geometry": {
+        "coordinates": [
+          -81.793489281406,
+          36.146305589826326
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Grandfather Center",
+        "desc": "",
+        "url": "https://ebird.org/checklist/S138687676"
+      },
+      "geometry": {
+        "coordinates": [
+          -81.83638839468709,
+          36.1178476108184
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Linville Gap wetlands",
+        "desc": "",
+        "url": "https://ebird.org/checklist/S138492308"
+      },
+      "geometry": {
+        "coordinates": [
+          -81.84225664393827,
+          36.118200221000734
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Tanglewood Cemetery Neighborhood",
+        "desc": "",
+        "url": "https://ebird.org/checklist/S138690015"
+      },
+      "geometry": {
+        "coordinates": [
+          -81.86695694594819,
+          36.07990136611106
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Fields below Linville VFD",
+        "desc": "",
+        "url": "https://ebird.org/checklist/S138690206"
+      },
+      "geometry": {
+        "coordinates": [
+          -81.86900088321005,
+          36.07776358025856
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Old Yonahlossee Road",
+        "desc": "",
+        "url": "https://ebird.org/checklist/S138690721"
+      },
+      "geometry": {
+        "coordinates": [
+          -81.85220034065108,
+          36.08076152292848
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Half Moon Overlook",
+        "desc": "",
+        "url": "https://ebird.org/checklist/S138502904"
+      },
+      "geometry": {
+        "coordinates": [
+          -81.84369320143885,
+          36.08851060308899
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Grandfather Mountain Swinging Bridge",
+        "desc": "",
+        "url": "https://ebird.org/checklist/S138502464"
+      },
+      "geometry": {
+        "coordinates": [
+          -81.83258634114455,
+          36.0947596866791
+        ],
+        "type": "Point"
+      }
+    }
+]};
